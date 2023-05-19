7fc23d219dbed979540f1d114b627efe86615073
n3eda <69794103+n3eda@users.noreply.github.com>
2023-05-19 13:35:40 +0200

    fixed wrong resolution

--- transcriber.py
+++ transcriber.py
@@ -100,7 +100,7 @@ def process():
 # Erstelle das Hauptfenster
 window = tk.Tk()
 window.title("Pr0-Videoübersetzer")
-window.geometry("400x700")
+window.geometry("400x750")
 
 # Erstelle Eingabefeld für URL
 url_file_label = tk.Label(window, text="URL/File:")
